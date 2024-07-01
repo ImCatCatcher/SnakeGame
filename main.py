b98b0708a5d257849fa78068408d33489e9f16e0
Arseniy Zasypkin <36149587+ImCatCatcher@users.noreply.github.com>
2024-07-01 13:46:39 +0300

    01.07 13:46

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ curX = randrange(0,400,20)
 curY = randrange(0,400,20)
 snake = [(curX, curY)] #координаты старта змейки
 
-dx, dy = 0, 0
+changeX, changeY = 0, 0
 fps = 10
 
 curDir = ""
@@ -53,16 +53,16 @@ while True:
 
     key = pg.key.get_pressed()
     if key[pg.K_w] and curDir != "w" and curDir != "s":
-        dx,dy = 0, -1
+        changeX,changeY = 0, -1
         curDir = "w"
     if key[pg.K_s] and curDir != "w" and curDir != "s":
-        dx,dy = 0, 1
+        changeX,changeY = 0, 1
         curDir = "s"
     if key[pg.K_a] and curDir != "a" and curDir != "d":
-        dx,dy = -1, 0
+        changeX,changeY = -1, 0
         curDir = "a"
     if key[pg.K_d] and curDir != "a" and curDir != "d":
-        dx,dy = 1, 0
+        changeX,changeY = 1, 0
         curDir = "d"
     
     for i, j in snake:
@@ -70,8 +70,8 @@ while True:
 
     pg.draw.rect(screen, pg.Color("red"),(bonus[0], bonus[1], 20, 20))
 
-    curX += dx * 20
-    curY += dy * 20
+    curX += changeX * 20
+    curY += changeY * 20
 
     if curX > 400: curX = 0
     if curX < 0:   curX = 400
